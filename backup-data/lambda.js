8c592a415f892807a4546a6bec91fc30a2a84b5b
AemieJ <aemie.j@hotmail.co.uk>
2021-04-08 21:41:24 +0400

    push changes: not working

--- backup-data/lambda.js
+++ backup-data/lambda.js
@@ -1,20 +1,33 @@
 'use strict';
 const AWS = require("aws-sdk");
 
-exports.handler = function (event, context) {
-    const s3 = new AWS.S3();
-    const sourceBucket = "entry-bucket-demo";
-    const destinationBucket = "backup-bucket-demo";
+const s3 = new AWS.S3();
 
-    const objectKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+const sourceBucket = "entry-bucket-demo";
+const destinationBucket = "backup-bucket-demo";
+
+exports.handler = async (event) => {
+    let response;
+    const objectKey = event.Records[0].s3.object.key;
     const copySource = encodeURI(sourceBucket + '/' + objectKey);
     const copyParams = { 
         Bucket: destinationBucket, 
         CopySource: copySource, 
         Key: objectKey 
     };
-    s3.copyObject(copyParams, function (err, data) {
-        if (err) console.log(err);
-        else console.log('Data has been backed up');
-    });
+
+    try {
+        let data = await s3.copyObject(copyParams).promise();
+        response = {
+            statusCode: 200,
+            body: data
+        };
+    } catch (err) {
+        response = {
+            statusCode: 500,
+            body: err
+        };
+    }
+
+    return response;
 };
